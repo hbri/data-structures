21097397cb62ebed65ae9d302407924a1a867255
Chris Cali <ChristopherCali1415@gmail.com>
2020-08-20 19:40:29 -0400

    Stub out Stack class using prototypal instantiation pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = Object.create(Stack.prototype);
+  // create property to make current object unique
+  //storage.counter = counter;
+
+  return storage;
+
 };
 
-var stackMethods = {};
+Stack.prototype.push = function() {
 
+};
+
+Stack.prototype.pop = function() {
+
+};
+
+Stack.prototype.size = function() {
+
+};
 
