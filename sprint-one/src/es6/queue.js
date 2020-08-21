af1bbfe3a92e8e67fdc6001f08e775b98de26c7b
Chris Cali <ChristopherCali1415@gmail.com>
2020-08-20 23:12:50 -0400

    Implement queue class using the ES6 instantiation pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,28 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.head = 0;
+    this.newIndex = 0;
+    this.count = 0;
   }
 
+  enqueue(value) {
+    this[this.newIndex] = value;
+    this.count++;
+    this.newIndex++;
+  }
+  dequeue() {
+    var tempVar = this[this.head];
+    delete this[this.head];
+    this.head++;
+    this.count--;
+    if (this.count === 0) {
+      this.head = 0;
+      this.newIndex = 0;
+    }
+    return tempVar;
+  }
+  size() {
+    return this.count < 0 ? 0 : this.count;
+  }
 }
