e8d0f8be1ede8561984305cf0fade0de0c4bb880
Chris Cali <ChristopherCali1415@gmail.com>
2020-08-20 22:37:57 -0400

    Implement queue class using the pseudoclassical instantiation pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.counter = 0;
+  this.head = 0;
+  this.newIndex = 0;
 };
 
-
+Queue.prototype = {
+  constructor: Queue,
+  enqueue: function(value) {
+    // Add value at the newIndex location
+    this[this.newIndex] = value;
+    this.newIndex++;
+    this.counter++;
+  },
+  dequeue: function() {
+    let tempVar = this[this.head];
+    delete this[this.head];
+    this.counter--;
+    this.head++;
+    return tempVar;
+  },
+  size: function() {
+    return this.counter < 0 ? 0 : this.counter;
+  }
+};
