f0e97324fd7e475813abc6908ea16572f2b9cbea
Chris Cali <ChristopherCali1415@gmail.com>
2020-08-20 22:44:20 -0400

    Implement stack class using ES6 instantiation pattern

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.counter = 0;
   }
 
+  // push
+  push(value) {
+    this[this.counter] = value;
+    this.counter++;
+  }
+
+  // pop
+  pop() {
+    let tempVal = this[this.counter - 1];
+    delete this[this.counter - 1];
+    this.counter--;
+    return tempVal;
+  }
+
+  //size
+  size() {
+    return this.counter < 0 ? 0 : this.counter;
+  }
 }
