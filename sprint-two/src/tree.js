dd5b20c2c90c702b42ceac3d7b81a05bc3f9cb47
Brian Hauck <brihauck4@gmail.com>
2020-08-22 10:55:57 -0700

    update tree.js to code to pass all tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,8 @@
 var Tree = function(value) {
   var newTree = {};
+  _.extend(newTree, treeMethods);
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,11 +10,40 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  //debugger;
+  // If currentNode.value === target
+  // Return true
+  // if (isValueTarget(this, target) === true) {
+  //   return true;
+  // }
+
+  // If currentNode.children.length is > 0
+  // Run the recursive function
+  if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      if (isValueTarget(this.children[i], target)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 };
 
+// Helper Recursive function
+var isValueTarget = function (node, target) {
+  if (node.value === target) {
+    return true;
+  } else {
+    for (var j = 0; j < node.children.length; j++) {
+      return isValueTarget(node.children[j], target);
+    }
+  }
+};
 
 
 /*
