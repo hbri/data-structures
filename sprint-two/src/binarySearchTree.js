db462c1c468722afb2b1e86f4a27cb85cc9d81d0
Brian Hauck <brihauck4@gmail.com>
2020-08-22 17:36:32 -0700

    finish code for hashTable, start code for binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,30 @@
 var BinarySearchTree = function(value) {
+  // debugger;
+  var binarySearchTree = {
+    left: null,
+    right: null,
+    value: value
+  };
+  _.extend(binarySearchTree, binaryMethods);
+
+  return binarySearchTree;
 };
 
+var binaryMethods = {
+  insert: function (value) {
+    var newTree = BinarySearchTree(value);
 
+  },
+
+  contains: function () {
+
+  },
+
+  depthFirstLog: function () {
+
+  }
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,42 @@
-
-
-var HashTable = function() {
+var HashTable = function () {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function (k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // Create bucket
+  // debugger;
+  if (this._storage[index]) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === k) {
+        this._storage[index][i][1] = v;
+      }
+    }
+    this._storage[index].push([k, v]);
+  } else {
+    this._storage[index] = [];
+    this._storage[index].push([k, v]);
+  }
 };
 
-HashTable.prototype.retrieve = function(k) {
+HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      return this._storage[index][i][1];
+    }
+  }
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (_.contains(this._storage[index][i], k)) {
+      this._storage[index][i].splice(_.indexOf(this._storage[index][i]), 1);
+    }
+  }
 };
 
 
