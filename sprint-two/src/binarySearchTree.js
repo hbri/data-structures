3c2fb2bd170c0c67ff3544792325c72d9da620bd
Brian Hauck <brihauck4@gmail.com>
2020-08-22 19:20:08 -0700

    finish code for binarySearchTree to pass all test

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -12,16 +12,46 @@ var BinarySearchTree = function(value) {
 
 var binaryMethods = {
   insert: function (value) {
-    var newTree = BinarySearchTree(value);
-
+    let newTree = BinarySearchTree(value);
+    // If value < this.value
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = newTree;
+      } else {
+        this.left.insert(value);
+      }
+      // If value > this.value
+    } else {
+      if (this.right === null) {
+        this.right = newTree;
+      } else {
+        this.right.insert(value);
+      }
+    }
   },
 
-  contains: function () {
+  contains: function (target) {
+    if (this.value === target) {
+      return true;
+    }
 
+    if (target < this.value && this.left !== null) {
+      return this.left.contains(target);
+    } else if (this.value !== target && this.right !== null) {
+      return this.right.contains(target);
+    }
+    return false;
   },
 
-  depthFirstLog: function () {
+  depthFirstLog: function (func) {
+    func(this.value);
+    if (this.left !== null) {
+      this.left.depthFirstLog(func);
+    }
 
+    if (this.right !== null) {
+      this.right.depthFirstLog(func);
+    }
   }
 };
 /*
